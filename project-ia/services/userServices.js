5a51c56a0b3c9c668e9d7ba65ca6079693343e94
Abd_El Rahman Mohamed <apdomedo6@gmail.com>
2023-05-08 22:15:18 +0300

    fixing update user

--- project-ia/services/userServices.js
+++ project-ia/services/userServices.js
@@ -55,8 +55,8 @@ exports.updateUser = asyncHandler(async (req, res) => {
   const hashedPassword = await bcrypt.hash(password, 10);
   const updated_at = new Date();
   db.query(
-    "UPDATE users SET name = ?, email = ?, password = ? , phone = ? ,role = ?,updated_at WHERE id = ?",
-    [name, email, hashedPassword, phone, role,updated_at, id],
+    "UPDATE users SET name = ?, email = ?, password = ? , phone = ? ,role = ?,updated_at=? WHERE id = ?",
+    [name, email, hashedPassword, phone, role, updated_at, id],
     (err, results) => {
       if (err) throw err;
       res.json({ message: "User updated successfully" });
